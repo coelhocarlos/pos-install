97790b7c3e504c63954386f8ad3d988a9aabe10f
carlos coelho <carloscoelho_@msn.com>
2018-06-11 18:45:22 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,23 @@
  #!/bin/bash
-declare PACKAGES=("apt-get install mysql"  "apt-get install php7")
- show_dialog()
-{
-    p=$PACKAGES             # percentage
-    date1=`date +%s`
-    while [ "$p" != 100 ]; do
-        read -t 1 tmp && p=$tmp
-        elapsed="$(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)"
-        echo $p | dialog --title "File upgrade" --gauge "Please wait...\n\n\n\n$elapsed" 10 70 0
-    done
-}
-
-task()              # fake task
-{ for p in `seq 1 100`; do echo $p; sleep 2; done; }
-
-task | show_dialog
+pass='apt-get install php7 apt-get mysql'
+chk='number14'
+max=0
+for i in $pass ; do
+    let max=$max+1
+done
+index=0
+strdone="#########################"
+strtodo="-------------------------"
+for i in $pass ; do
+    let index=$index+1
+    if [ "$i" == "$chk" ]; then
+        echo ""
+        echo ' Found ^_^'
+    else
+        lendone=$((index * ${#strdone} / max))
+        let lentodo=${#strdone}-$lendone
+        percent=$((index * 100 / max))
+        echo -ne "[#${strdone:0:$lendone}${strtodo:0:$lentodo}] $percent%\r"
+    fi
+    sleep 1
+done
