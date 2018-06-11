2cf28689674c547e6810a33d05f49744f68a2d03
carlos coelho <carloscoelho_@msn.com>
2018-06-11 19:01:32 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,21 @@
-#!/bin/bash
 
+#!/bin/bash
+# dvdcopy.sh - A sample shell script to display a progress bar
+# set counter to 0 
 set -eu -o pipefail # fail on error , debug all lines
 
 sudo -n true
 test $? -eq 0 || exit 1 "you should have sudo priveledge to run this script"
 
 echo installing the must-have pre-requisites
-while read -r p ; do sudo apt-get install -y $p ; done < <(cat << "EOF"
-    perl
+counter=0
+(
+# set infinite while loop
+while :
+do
+while read -r p ; do sudo apt-get install -y $p ; done < <( cat <<"EOF"
+$counter
+perl
     zip unzip
     exuberant-ctags
     mutt
@@ -17,14 +25,15 @@ while read -r p ; do sudo apt-get install -y $p ; done < <(cat << "EOF"
     curl
     wget
     libwww-curl-perl
+    EOF
+ ( $counter%):
+XXX
 EOF
-)
-
-echo installing the nice-to-have pre-requisites
-echo you have 5 seconds to proceed ...
-echo or
-echo hit Ctrl+C to quit
-echo -e "\n"
-sleep 6
-
-sudo apt-get install -y tig
+# increase counter by 10
+(( counter+=10 ))
+[ $counter -eq 100 ] && break
+# delay it a specified amount of time i.e 1 sec
+sleep 1
+done
+) |
+dialog --title "File Copy" --gauge "Please wait" 7 70 0
