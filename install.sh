152a11320e45ee37ba2d8df768dbc5878b248ea1
carlos coelho <carloscoelho_@msn.com>
2018-06-15 08:50:04 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,8 @@ if [[ -r /etc/os-release ]]; then
     if [[ $ID = ubuntu ]]; then
         read _ UBUNTU_VERSION_NAME <<< "$VERSION"
         echo "Running Ubuntu $UBUNTU_VERSION_NAME"
-tput setb 5
+#color screen
+#tput setb 5
 # clear the screen
 tput clear
 
@@ -30,23 +31,90 @@ echo "M A I N - M E N U"
 tput sgr0
  
 tput cup 7 15
-echo "1. User Management"
+echo "1 User Management"
  
 tput cup 8 15
-echo "2. Service Management"
+echo "2 Service Management"
  
 tput cup 9 15
-echo "3. Process Management"
+echo "3 Process Management"
  
 tput cup 10 15
-echo "4. Backup"
+echo "4 Backup"
  
 # Set bold mode
 tput bold
 tput cup 12 15
 read -p "Enter your choice [1-4] " choice
+echo ""
 
- 
+echo "Select item: "
+
+#loop around gathering input until QUIT is more than 0
+
+QUIT=0
+
+while [ $QUIT -lt 1 ]
+
+do
+
+   #Move cursor to after select message
+
+   tput cup 8 13
+
+   #Delete from cursor to end of line
+
+   tput el
+
+   read SEL
+
+   if [ ${#SEL} -lt 1 ]
+
+   then
+
+      continue
+
+   fi
+
+   if [ $SEL -eq 4 ]
+
+   then
+
+      QUIT=1
+
+      continue
+
+   fi
+
+   #put message in middle of screen
+
+   tput cup 15 20
+
+   #Delete from cursor to end of line
+
+   tput el
+
+   case $SEL in
+
+      *) echo "You selected $SEL";;
+
+   esac
+
+done
+
+#reset the screen
+
+#Find out if this is a "linux" virtual terminal
+
+if [ $TERM ~ "linux" ]
+
+then
+
+     tput setb 0 #reset background to black
+
+fi
+
+tput reset 
 tput clear
 tput sgr0
 tput rc
